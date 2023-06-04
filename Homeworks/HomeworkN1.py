7a2001702ebf76419154236e142a58871dce1fcc
DitoTalakhadze <ditotalaxadze@gmail.com>
2023-06-04 20:32:02 +0400

    add homework 1

---
+++ Homeworks/HomeworkN1.py
@@ -0,0 +1,12 @@
+"""Quiz
+Part 1"""
+name = "Dito"
+age = 25 
+print(name)
+print(age)
+bio = F'{name}, {age}'
+print(bio)
+# Part 2 
+country = "liberia"
+population = 5 
+print(F"{country.title()}'s population is {population} million")
