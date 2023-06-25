68ee852fdae42d989d875019680f9004d82fce3d
DitoTalakhadze <ditotalaxadze@gmail.com>
2023-06-25 21:47:45 +0400

    django, hello world app

--- Classwork copy/python110-main/workshop_6/args_and_kwargs.py
+++ Classwork copy/python110-main/workshop_6/args_and_kwargs.py
@@ -1,13 +1,10 @@
 def my_func(*args: int, **kwargs: int) -> None:
     if len(args) < 2:
-        raise Exception('I need at least 2 parameters')
+        raise Exception("I need at least 2 parameters")
     print(args, kwargs)
 
 
-my_kwargs: dict[str, int] = {
-    'a': 8,
-    'b': 9
-}
+my_kwargs: dict[str, int] = {"a": 8, "b": 9}
 
 nums: list[int] = [2, 5, 8, 5, 6, 3, 2]
 
